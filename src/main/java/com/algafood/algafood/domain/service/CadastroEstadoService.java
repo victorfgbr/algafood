93c7146dd6a6d713072243e03cb888176175411d
Victor Goulart <victorfgbr@gmail.com>
2020-12-11 23:57:14 -0300

    flush ao exlcluir estado para lançar exception de conflito se houver

--- src/main/java/com/algafood/algafood/domain/service/CadastroEstadoService.java
+++ src/main/java/com/algafood/algafood/domain/service/CadastroEstadoService.java
@@ -29,6 +29,7 @@ public class CadastroEstadoService {
 	public void remover(Long id) {
 		try {
 			estadoRepository.deleteById(id);
+			estadoRepository.flush();
 		} catch (EmptyResultDataAccessException e) {
 			throw new EstadoNaoEncontradoException(id);
 
